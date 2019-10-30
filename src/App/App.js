4b900c014c8c5d0305ae63c75cba90e1ad1bdeb5
“Elyse <elysemyers1@gmail.com>
2019-10-30 17:51:18 -0600

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <p>Beauty Box</p>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './App/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { loadState, saveState } from './localStorage/localStorage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// store.subscribe(() => {
+//   saveState({
+//     user: store.getState().user
+//   });
+// });
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+    <App />
+    </Router>
+  </Provider>, 
+  document.getElementById('root'));
+
 serviceWorker.unregister();
