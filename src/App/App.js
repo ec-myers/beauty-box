74744be1e26f0310117e8f2598a09feb953e4189
“Elyse <elysemyers1@gmail.com>
2019-10-31 11:14:54 -0600

    Adds functionality to fetch lipstick products and add to global store in componentDidMount

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
+import { getProduct } from '../util/apiCalls';
+import { setLipsticks } from '../actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-function App() {
-  return (
-    <p>Beauty Box</p>
-  );
+class App extends Component {
+  componentDidMount = async() => {
+    const { setLipsticks } = this.props;
+    try{
+      const lipsticks = await getProduct('lipstick');
+      console.log(lipsticks)
+      setLipsticks(lipsticks);
+
+    } catch ({message}){
+      console.log(message)
+    }
+  }
+
+  render() {
+    return(
+      <p>Beauty Box</p>
+    )
+  }
 }
 
-export default App;
+export const mapStateToProps = ({ lipsticks }) => ({
+  lipsticks
+});
+
+export const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ setLipsticks }, dispatch) 
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
