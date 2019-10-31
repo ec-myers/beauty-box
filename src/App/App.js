d98ef2f0bea5a905528cd3b40936151ed5feb0eb
“Elyse <elysemyers1@gmail.com>
2019-10-31 14:29:23 -0600

    Adds functionality to fetch foundation  products and add to global store in componentDidMount

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { getProduct } from '../util/apiCalls';
-import { setLipsticks, setMascaras } from '../actions';
+import { setLipsticks, setMascaras, setFoundations } from '../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class App extends Component {
   componentDidMount = async() => {
-    const { setLipsticks, setMascaras } = this.props;
+    const { setLipsticks, setMascaras, setFoundations } = this.props;
     try{
       const lipsticks = await getProduct('lipstick');
       setLipsticks(lipsticks);
       const mascaras = await getProduct('mascara');
       setMascaras(mascaras);
-
+      const foundations = await getProduct('foundation');
+      setFoundations(foundations);
     } catch ({message}){
       console.log(message)
     }
@@ -26,13 +27,14 @@ class App extends Component {
   }
 }
 
-export const mapStateToProps = ({ lipsticks, mascaras }) => ({
+export const mapStateToProps = ({ lipsticks, mascaras, foundations }) => ({
   lipsticks,
-  mascaras
+  mascaras,
+  foundations
 });
 
 export const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ setLipsticks, setMascaras }, dispatch) 
+  return bindActionCreators({ setLipsticks, setMascaras, setFoundations }, dispatch) 
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
