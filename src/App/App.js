4144d6017a3994bfc7d6fce72ebca1583f7cda83
“Elyse <elysemyers1@gmail.com>
2019-11-01 09:43:52 -0600

    Adds functionality to fetch eyeshadow products and add to global store in componentDidMount

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { getProduct } from '../util/apiCalls';
-import { setLipsticks, setMascaras, setFoundations, setBlushes } from '../actions';
+import { setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows } from '../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class App extends Component {
   componentDidMount = async() => {
-    const { setLipsticks, setMascaras, setFoundations, setBlushes } = this.props;
+    const { setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows } = this.props;
     try {
       const lipsticks = await getProduct('lipstick');
       setLipsticks(lipsticks);
@@ -17,6 +17,8 @@ class App extends Component {
       setFoundations(foundations);
       const blushes = await getProduct('blush');
       setBlushes(blushes);
+      const eyeshadows = await getProduct('eyeshadow');
+      setEyeshadows(eyeshadows);
     } catch ({message}){
       console.log(message)
     }
@@ -29,15 +31,16 @@ class App extends Component {
   }
 }
 
-export const mapStateToProps = ({ lipsticks, mascaras, foundations, blushes }) => ({
+export const mapStateToProps = ({ lipsticks, mascaras, foundations, blushes, eyeshadows }) => ({
   lipsticks,
   mascaras,
   foundations,
-  blushes
+  blushes,
+  eyeshadows
 });
 
 export const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ setLipsticks, setMascaras, setFoundations, setBlushes }, dispatch) 
+  return bindActionCreators({ setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows }, dispatch) 
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
