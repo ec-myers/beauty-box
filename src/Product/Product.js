522eafa9ee50e5e223b84e54dbdd766127427cb6
“Elyse <elysemyers1@gmail.com>
2019-11-02 15:47:02 -0600

    Adds functionality to display default image if provided image link returns a 404 error

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './Product.scss';
 
-const Product = () => {
+const Product = ({ product }) => {
+  let tagList = product.tag_list.map(tag => <li>{tag}</li>);
+
+  // addDefaultSrc(e) {
+  //   e.target.src = '../Images/default_image.jpg';
+  // }
+  
   return (
     <div className='Product'>
-      <img className='product-img'></img>
-      <h3></h3>
-      <h4></h4>
-      <p></p>
-      <ul></ul>
+      <object className='product-img' data={product.image_link} type='image/jpg' alt={product.name}>
+        <img src={require('../Images/default_image.jpg')} alt='default'/>
+      </object>
+      <h3>{product.brand}</h3>
+      <h4>{product.name}</h4>
+      <p>{product.description}</p>
+      <ul>{ tagList }</ul>
       <footer>
-        <button>Shop</button>
-        <img></img>
+        <a href={product.product_link}>Shop</a>
+        <button>ADD TO COLLECTION</button>
       </footer>
     </div>
   )
