3037c64bdbd7af155c7d37638b43c063ab9f4e0d
“Elyse <elysemyers1@gmail.com>
2019-11-02 14:20:20 -0600

    Adds updated links to products pages to utilize match

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -15,27 +15,27 @@ const Category = () => {
         <img src={require('../Images/hero_image_right.jpg')} alt='perfume on a blanket'></img>
     </div>
     <div className='categories-div'>
-      <Link to={ '/foundation'}> 
+      <Link to={ '/products/foundations'}> 
         <div className='foundation-div'>
           <h4>FOUNDATION</h4>
         </div> 
       </Link>
-      <Link to='/blush'>
+      <Link to='/products/blushes'>
         <div className='blush-div'>
           <h4>BLUSH</h4>
         </div>
       </Link>
-      <Link to='/mascara'>
+      <Link to='/products/mascaras'>
         <div className='mascara-div'>
           <h4>MASCARA</h4>
         </div>
       </Link>
-      <Link to='/eyeshadow'>
+      <Link to='/products/eyeshadows'>
         <div className='eyeshadow-div'>
           <h4>EYESHADOW</h4>
         </div>
       </Link>
-      <Link to='/lipstick'>
+      <Link to='/products/lipsticks'>
         <div className='lipstick-div'>
           <h4>LIPSTICK</h4>
         </div>
