d5e9842932fc8928e74d216c70d29d180f78641b
“Elyse <elysemyers1@gmail.com>
2019-11-02 13:10:52 -0600

    Adds links to each category page within Category component

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Category.scss';
 
 const Category = () => {
@@ -14,21 +15,31 @@ const Category = () => {
         <img src={require('../Images/hero_image_right.jpg')} alt='perfume on a blanket'></img>
     </div>
     <div className='categories-div'>
-      <div className='foundation-div'>
-        <h4>FOUNDATION</h4>
-      </div>
-      <div className='blush-div'>
-        <h4>BLUSH</h4>
-      </div>
-      <div className='mascara-div'>
-        <h4>MASCARA</h4>
-      </div>
-      <div className='eyeshadow-div'>
-        <h4>EYESHADOW</h4>
-      </div>
-      <div className='lipstick-div'>
-        <h4>LIPSTICK</h4>
-      </div>
+      <Link to={ '/foundation'}> 
+        <div className='foundation-div'>
+          <h4>FOUNDATION</h4>
+        </div> 
+      </Link>
+      <Link to='/blush'>
+        <div className='blush-div'>
+          <h4>BLUSH</h4>
+        </div>
+      </Link>
+      <Link to='/mascara'>
+        <div className='mascara-div'>
+          <h4>MASCARA</h4>
+        </div>
+      </Link>
+      <Link to='/eyeshadow'>
+        <div className='eyeshadow-div'>
+          <h4>EYESHADOW</h4>
+        </div>
+      </Link>
+      <Link to='/lipstick'>
+        <div className='lipstick-div'>
+          <h4>LIPSTICK</h4>
+        </div>
+      </Link>
     </div>
     </>
   )
