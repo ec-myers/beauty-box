95e950b0af6afd6e6b7652644255820a511900d4
“Elyse <elysemyers1@gmail.com>
2019-11-02 13:12:22 -0600

    Adds routes for each category in App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Nav from '../Nav/Nav';
+import Category from '../Category/Category';
+import Container from '../Container/Container';
 import { getProduct } from '../util/apiCalls';
 import { setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows } from '../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   componentDidMount = async() => {
     const { setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows } = this.props;
     try {
-      const lipsticks = await getProduct('lipstick');
-      setLipsticks(lipsticks);
-      const mascaras = await getProduct('mascara');
-      setMascaras(mascaras);
-      const foundations = await getProduct('foundation');
-      setFoundations(foundations);
-      const blushes = await getProduct('blush');
-      setBlushes(blushes);
-      const eyeshadows = await getProduct('eyeshadow');
-      setEyeshadows(eyeshadows);
+      // const lipsticks = await getProduct('lipstick');
+      // setLipsticks(lipsticks);
+      // const mascaras = await getProduct('mascara');
+      // setMascaras(mascaras);
+      // const foundations = await getProduct('foundation');
+      // setFoundations(foundations);
+      // const blushes = await getProduct('blush');
+      // setBlushes(blushes);
+      // const eyeshadows = await getProduct('eyeshadow');
+      // setEyeshadows(eyeshadows);
     } catch ({message}){
       console.log(message)
     }
@@ -26,7 +30,15 @@ class App extends Component {
 
   render() {
     return(
-      <p>Beauty Box</p>
+      <>
+        <Nav />
+        <Category />
+        <Route exact path='/foundation' render={() => <Container />}/>
+        <Route exact path='/blush' render={() => <Container />} />
+        <Route exact path='/mascara' render={() => <Container />} />
+        <Route exact path='/eyeshadow' render={() => <Container />} />
+        <Route exact path='/lipstick' render={() => <Container />} />
+      </>
     )
   }
 }
