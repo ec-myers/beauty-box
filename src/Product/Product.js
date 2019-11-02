f2fe6ea5b8420ff531a699e170fdc6e7c8f66ea8
“Elyse <elysemyers1@gmail.com>
2019-11-02 12:49:44 -0600

    Adds files and basic Product component setup

---
+++ src/Product/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Product.scss';
+
+const Product = () => {
+  return (
+    <div className='Product'>
+      <img className='product-img'></img>
+      <h3></h3>
+      <h4></h4>
+      <p></p>
+      <ul></ul>
+      <footer>
+        <button>Shop</button>
+        <img></img>
+      </footer>
+    </div>
+  )
+}
+
+export default Product;
