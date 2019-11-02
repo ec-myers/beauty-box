9163e09d9ebc4ab47622a5b57abe39f9d0029ddf
“Elyse <elysemyers1@gmail.com>
2019-11-01 18:25:27 -0600

    Adds styling and structure to hero image and category section

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -3,12 +3,34 @@ import './Category.scss';
 
 const Category = () => {
   return (
+    <>
     <div className='hero-div'>
       <img src={require('../Images/hero_img_left.jpg')} alt='woman smiling at the camera'/>
-      <div>
+      <div className='shop-div'>
+        <h3>EXPLORE</h3>
+        <h3>BEAUTY</h3>
+        <button>SHOP ALL</button>
       </div>
-      <img src={require('../Images/hero_image_right.jpg')} alt='perfume on a blanket'></img>
+        <img src={require('../Images/hero_image_right.jpg')} alt='perfume on a blanket'></img>
     </div>
+    <div className='categories-div'>
+      <div className='foundation-div'>
+        <h4>FOUNDATION</h4>
+      </div>
+      <div className='blush-div'>
+        <h4>BLUSH</h4>
+      </div>
+      <div className='mascara-div'>
+        <h4>MASCARA</h4>
+      </div>
+      <div className='eyeshadow-div'>
+        <h4>EYESHADOW</h4>
+      </div>
+      <div className='lipstick-div'>
+        <h4>LIPSTICK</h4>
+      </div>
+    </div>
+    </>
   )
 }
 
