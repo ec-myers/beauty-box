2cbebb9fc379264fab36f9f44199ea2cf7f96fa8
“Elyse <elysemyers1@gmail.com>
2019-11-02 14:22:04 -0600

    Adds functionality to utilize match in product route for each product category

--- src/App/App.js
+++ src/App/App.js
@@ -13,16 +13,16 @@ class App extends Component {
   componentDidMount = async() => {
     const { setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows } = this.props;
     try {
-      // const lipsticks = await getProduct('lipstick');
-      // setLipsticks(lipsticks);
-      // const mascaras = await getProduct('mascara');
-      // setMascaras(mascaras);
-      // const foundations = await getProduct('foundation');
-      // setFoundations(foundations);
-      // const blushes = await getProduct('blush');
-      // setBlushes(blushes);
-      // const eyeshadows = await getProduct('eyeshadow');
-      // setEyeshadows(eyeshadows);
+      const lipsticks = await getProduct('lipstick');
+      setLipsticks(lipsticks);
+      const mascaras = await getProduct('mascara');
+      setMascaras(mascaras);
+      const foundations = await getProduct('foundation');
+      setFoundations(foundations);
+      const blushes = await getProduct('blush');
+      setBlushes(blushes);
+      const eyeshadows = await getProduct('eyeshadow');
+      setEyeshadows(eyeshadows);
     } catch ({message}){
       console.log(message)
     }
@@ -32,12 +32,19 @@ class App extends Component {
     return(
       <>
         <Nav />
-        <Category />
-        <Route exact path='/foundation' render={() => <Container />}/>
-        <Route exact path='/blush' render={() => <Container />} />
+        <Route exact path='/' render={() => <Category />} />
+        <Route exact path='/products/:type' render={({match}) => {
+          let productType = Object.keys(this.props).find(type => {
+            console.log('match.params.type', match.params.type )
+            console.log('type',type)
+            return type === match.params.type})
+          console.log('productType',productType)
+          console.log(this.props[productType])
+        return <Container productType={this.props[productType]}/>}}/>
+        {/* <Route exact path='/blush' render={() => <Container />} />
         <Route exact path='/mascara' render={() => <Container />} />
         <Route exact path='/eyeshadow' render={() => <Container />} />
-        <Route exact path='/lipstick' render={() => <Container />} />
+        <Route exact path='/lipstick' render={() => <Container />} /> */}
       </>
     )
   }
