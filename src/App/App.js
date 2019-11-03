54058bcee1e83e0d2b426f6aead27cd14a63e44c
“Elyse <elysemyers1@gmail.com>
2019-11-02 19:35:52 -0600

    Adds functionality to remove a product from collection

--- src/App/App.js
+++ src/App/App.js
@@ -13,16 +13,16 @@ class App extends Component {
   componentDidMount = async() => {
     const { setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows } = this.props;
     try {
-      // const lipsticks = await getProduct('lipstick');
-      // setLipsticks(lipsticks);
-      // const mascaras = await getProduct('mascara');
-      // setMascaras(mascaras);
-      // const foundations = await getProduct('foundation');
-      // setFoundations(foundations);
-      // const blushes = await getProduct('blush');
-      // setBlushes(blushes);
-      // const eyeshadows = await getProduct('eyeshadow');
-      // setEyeshadows(eyeshadows);
+      const lipsticks = await getProduct('lipstick');
+      setLipsticks(lipsticks);
+      const mascaras = await getProduct('mascara');
+      setMascaras(mascaras);
+      const foundations = await getProduct('foundation');
+      setFoundations(foundations);
+      const blushes = await getProduct('blush');
+      setBlushes(blushes);
+      const eyeshadows = await getProduct('eyeshadow');
+      setEyeshadows(eyeshadows);
     } catch ({message}){
       console.log(message)
     }
@@ -30,13 +30,18 @@ class App extends Component {
 
   toggleCollection = (product) => {
     const { collection } = this.props;
-
     collection.map(product => product.id).includes(product.id) ? this.removeProduct(product) : this.addProduct(product);
   }
 
   addProduct = (product) => {
-    const { collection } = this.props;
+    const { collection, setCollection } = this.props;
     let newCollection = [...collection, product];
+    setCollection(newCollection);
+  }
+
+  removeProduct = (product) => {
+    const { collection, setCollection } = this.props;
+    let newCollection = collection.filter(savedProduct => savedProduct.id !== product.id);
     setCollection(newCollection);
   }
 
