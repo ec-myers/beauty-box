9744b9434ef5e174f979a63922ad1ebeeba9f572
“Elyse <elysemyers1@gmail.com>
2019-11-03 14:56:59 -0700

    Adds test to check that result is correct if type matches SET_ERROR

--- src/reducers/error.test.js
+++ src/reducers/error.test.js
@@ -7,4 +7,15 @@ describe('error', () => {
 
     expect(result).toEqual(expected);
   });
+
+  it('should match case SET_ERROR if action type is correct', () => {
+    const initialState = '';
+    const action = {
+      type: 'SET_ERROR',
+      error: 'error'
+    };
+    const result = 'error';
+
+    expect(error(initialState, action)).toEqual(result);
+  });
 });
