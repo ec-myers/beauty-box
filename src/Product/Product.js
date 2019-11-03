be50b6c46bbafb0bb8e0190ae637324d5543f423
“Elyse <elysemyers1@gmail.com>
2019-11-02 19:47:41 -0600

    Adds functionality to destructure props and add toggleCollection method to button

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import './Product.scss';
 
-const Product = ({ product }) => {
+const Product = ({ product, toggleCollection }) => {
   let tagList = product.tag_list.map(tag => <li>{tag}</li>);
+  let { brand, name, description, product_link, id} = product;
 
-  // addDefaultSrc(e) {
-  //   e.target.src = '../Images/default_image.jpg';
-  // }
-  
   return (
     <div className='Product'>
       <div className='image-div'>
-        <object className='product-img' data={product.image_link} type='image/jpg' alt={product.name}>
+        <object className='product-img' data={product.image_link} type='image/jpg' alt={name}>
           <img className='product-img' src={require('../Images/default_image.jpg')} alt='default' />
         </object>
       </div>
       <div className='border-div'></div>
-      <h3>{product.brand}</h3>
-      <h4>{product.name}</h4>
-      <p>{product.description}</p>
+      <h3>{brand}</h3>
+      <h4>{name}</h4>
+      <p>{description}</p>
       <ul className='tag-list'>{tagList}</ul>
       <footer>
         <div className='shop-div'>
-          < a href={product.product_link}>SHOP</a>
+          < a href={product_link}>SHOP</a>
         </div>
-        <button>ADD</button>
+        <button onClick={() => toggleCollection(product)}>ADD</button>
       </footer>
     </div>
   )
