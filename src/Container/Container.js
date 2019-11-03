03100228a4a504590e50f24d2b78b3405d2d5c5b
“Elyse <elysemyers1@gmail.com>
2019-11-02 19:46:02 -0600

    Adds conditional logic to render collection products or product page details dependant on route

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -3,14 +3,18 @@ import { PropTypes } from 'prop-types';
 import Product from '../Product/Product';
 import './Container.scss';
 
-const Container = ({productType}) => {
-  const productList = productType.map(product => <Product key={product.id}product={product}/>)
-
-  return (
-    <main className = 'Container'>
-      {productList}
-    </main>
-  )
+const Container = ({productType, toggleCollection, type, collection}) => {
+  if (type === 'collection') {
+    return (collection.map(product => <Product key={product.id} product={product} toggleCollection={toggleCollection} /> ))
+  } else {
+    const productList = productType.map(product => <Product key={product.id}product={product} toggleCollection={toggleCollection} />);
+    
+    return (
+      <main className = 'Container'>
+        {productList}
+      </main>
+    )
+  }
 }
 
 export default Container;
