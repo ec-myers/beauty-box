0c15ee3aeecbd9019ebbec38c21390c0373b6f36
“Elyse <elysemyers1@gmail.com>
2019-11-02 20:40:50 -0600

    Adds functionality to conditionally render category links based on loading value

--- src/Category/Category.js
+++ src/Category/Category.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './Category.scss';
 
-const Category = () => {
+const Category = ({ isLoading }) => {
   return (
     <>
     <div className='hero-div'>
@@ -14,7 +15,8 @@ const Category = () => {
       </div>
         <img src={require('../Images/hero_image_right.jpg')} alt='perfume on a blanket'></img>
     </div>
-    <div className='categories-div'>
+    {isLoading && <img className='loading-img' src={require('../Images/loading.gif')} alt='loading' />}
+    {!isLoading && <div className='categories-div'>
       <Link to={ '/products/foundations'}> 
         <div className='foundation-div'>
           <h4>FOUNDATION</h4>
@@ -40,9 +42,13 @@ const Category = () => {
           <h4>LIPSTICK</h4>
         </div>
       </Link>
-    </div>
+    </div>}
     </>
   )
 }
 
-export default Category;
+export const mapStateToProps = ({ isLoading }) => ({
+  isLoading
+})
+
+export default connect(mapStateToProps)(Category);
