58d1ba63b84db9dc8b7333e23720bd0b12ab47d8
“Elyse <elysemyers1@gmail.com>
2019-11-03 15:16:50 -0700

    Adds test to check that correct result is returned if action type matches SET_MASCARAS

--- src/reducers/mascaras.test.js
+++ src/reducers/mascaras.test.js
@@ -7,4 +7,15 @@ describe('mascaras', () => {
 
     expect(result).toEqual(expected);
   });
+
+  it('should match case SET_MASCARAS if action type is correct', () => {
+    const initialState = [];
+    const action = {
+      type: 'SET_MASCARAS',
+      mascaras: [{}, {}, {}]
+    };
+    const result = [{}, {}, {}];
+
+    expect(mascaras(initialState, action)).toEqual(result);
+  });
 });
