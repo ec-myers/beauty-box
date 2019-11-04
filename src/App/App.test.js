7526b5ccd6b390df7d61d0755f8163d055a42322
“Elyse <elysemyers1@gmail.com>
2019-11-03 17:46:43 -0700

    Adds test to check value of mapStateToProps

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
+import { setLipsticks, setMascaras, setFoundations, setBlushes, setEyeshadows, setCollection, setError, setLoading } from '../actions/index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.mock('../util/apiCalls.js');
+
+describe('App', () => {
+ it.skip('should match the snapshot', () => {
+  let wrapper = shallow(<App />);
+
+  expect(wrapper).toMatchSnapshot();
+ });
+});
+
+describe('mapStateToProps', () => {
+  it('should return an object with 8 properties', () => {
+    let lipsticks, mascaras, foundations, blushes, eyeshadows, collection, error, isLoading;
+    let mockState = {
+      lipsticks,
+      mascaras,
+      foundations,
+      blushes,
+      eyeshadows,
+      collection,
+      error,
+      isLoading
+    };
+
+    let expected = {
+      lipsticks,
+      mascaras,
+      foundations,
+      blushes,
+      eyeshadows,
+      collection,
+      error,
+      isLoading
+    };
+
+    const mappedProps = mapStateToProps(mockState);
+
+    expect(mappedProps).toEqual(expected);
+  });
 });
