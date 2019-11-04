6fb83926458f5426d4528072b7b1c1e83b05643a
“Elyse <elysemyers1@gmail.com>
2019-11-03 19:07:55 -0700

    Adds proptypes for Container component

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -32,3 +32,11 @@ const Container = ({productType, toggleCollection, type, collection , allCategor
 }
 
 export default Container;
+
+Container.propTypes = {
+  productType: PropTypes.array,
+  toggleCollection: Proptypes.func,
+  type: PropTypes.string,
+  collection: PropTypes.array,
+  allCategories: PropTypes.array
+}
